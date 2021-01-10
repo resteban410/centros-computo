58bb9eaea1e3a6753d24a5f3348db5d2fa3ee42a
dorsai0611 <resteban410@gmail.com>
2021-01-10 14:24:00 -0600

    Codigo
    Esta es la prueba inicial del codigo en laravel

--- Sistema Control Centros Computo (codigo)/SCCC/pagina/ejemplo -.php
+++
@@ -1,7 +0,0 @@
-SELECT * 
-FROM prestamo AS pr, docente AS dc
-WHERE pr.docente_clave = dc.clave
-LIMIT 0 , 30
-
-
-SELECT fecha_prestamo,hora,HoraTermino,nombre, apellidos FROM prestamo INNER JOIN docente ON prestamo.docente_clave = docente.clave
