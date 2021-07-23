c6fe085deb6555c9c55506ede32a72b85a106a83
fernandalvarezarenas <fernanda.alvarez.arenas@gmail.com>
2021-07-22 19:48:24 -0500

    Cambios
    Cambios

---
+++ centros_computo_2/resources/views/alumnos.blade.php
@@ -0,0 +1,270 @@
+@extends('layouts.plantillabase')
+@section('contenido')
+<br></br>
+
+<!-- Modal Crear nuevo alumno-->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Nuevo registro</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    <form action="{{route('altalum')}}" method="post">
+      <div class="modal-body">       
+                @csrf
+            <div class="form-group">
+                <label>Matricula:</label>
+                    <input type="text" name="matricula" class= "form-control" placeholder="Escriba la matricula">
+                    {!! $errors->first('matricula', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Nombre:</label>
+                    <input type="text" name="nombre" class= "form-control" placeholder="Escriba el nombre">
+                    {!! $errors->first('nombre', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Apellido:</label>
+                    <input type="text" name="apellido" class= "form-control" placeholder="Escriba los apellidos"> 
+                    {!! $errors->first('apellido', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Carrera:</label>
+                       <select name="carrera">
+                          <option value="ISTII">Ingeniería en Sistemas y Tecnología de la Información Industrial</option>
+                          <option value="IPGI">Ingeniería en Procesos y Gestión Industrial</option>
+                          <option value="IAA">Ingeniería en Automatización Autotrónica </option>
+                        </select>                                      
+                    {!! $errors->first('carrera', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Correo Electronico:</label>
+                       <input type="email" name="correo_electronico" class="form-control" placeholder="Ej.:usuario@servidor.com">
+                    {!! $errors->first('correo_electronico', '<small>:message</small><br>') !!}
+            </div>  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+<!-- Termina modal de nuevo alumno -->
+
+
+
+<!-- Empieza modal de editar alumno -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Editar registro</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    <form action="{{route('editaralumno')}}" method="post" id="editForm">
+      <div class="modal-body">       
+                @csrf
+                @method('PUT')
+            <div class="form-group">
+                <label>Matricula:</label>
+                    <input type="text" name="matricula" id="matricula" class= "form-control" placeholder="Escriba la matricula">
+                    {!! $errors->first('matricula', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Nombre:</label>
+                    <input type="text" name="nombre" id="nombre" class= "form-control" placeholder="Escriba el nombre">
+                    {!! $errors->first('nombre', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Apellido:</label>
+                    <input type="text" name="apellido" id="apellido" class= "form-control" placeholder="Escriba los apellidos"> 
+                    {!! $errors->first('apellido', '<small>:message</small><br>') !!}
+            </div>
+
+            <div class="form-group">
+                <label>Carrera:</label>
+                       <select name="carrera" id="carrera">
+                          <option value="ISTII">Ingenieria en Sistemas y Tecnologias de la Información Industrial</option>
+                          <option value="IPGI">Ingeniería en Procesos y Gestión Industrial</option>
+                          <option value="IAA">Ingeniería en Autotronica </option>
+                        </select>  
+                        {!! $errors->first('carrera', '<small>:message</small><br>') !!}                                    
+            </div>
+
+            <div class="form-group">
+                <label>Correo Electronico:</label>
+                       <input type="email" name="correo_electronico" id="correo_electronico" class="form-control" placeholder="Ej.:usuario@servidor.com">
+                       {!! $errors->first('correo_electronico', '<small>:message</small><br>') !!}
+            </div>  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+<!--Termina modal de editar alumno-->
+
+
+
+<!-- Empieza modal de eliminar alumno -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Eliminar registro</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    <form action="{{route('borraralumno')}}" method="POST" id="deleteForm">
+      @csrf
+      @method('DELETE')
+      <div class="modal-body">       
+            <input type="hidden" name="_method" value="DELETE">
+        <p>¿Estas seguro de borrar este registro? Esta acción no puede revertirse. Para asegurarse, vuelva a escribir la matricula del alumno. </p>
+
+            <div class="form-group">
+                <label>Matricula:</label>
+                    <input type="text" name="matricula" id="matricula" class= "form-control" placeholder="Escriba la matricula">
+                    {!! $errors->first('matricula', '<small>:message</small><br>') !!}
+            </div>        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Borrar registro</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+<!--Termina modal de eliminar alumno-->
+
+<div class="container">
+    @if(count($errors) > 0)
+    <div class="alert alert-danger">
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>
+            {{$error}}
+        </li>
+        @endforeach
+    </ul>
+    </div>
+    @endif
+
+    @if(\Session::has('sucess'))
+        <div class="alert alert-success">
+            <p>{{\Session::get('sucess')}}</p>
+        </div>
+    @endif
+
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Agregar nuevo registro
+    </button>
+
+</div>
+
+<br></br>
+    <div class="card"> 
+        <div class="card-body">
+        <h4> Alumnos registrados</h4>
+        <table id="alumnos" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+            <tr>
+                <th>Matricula</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Carrera</th>
+                <th>Correo Electronico</th>
+                <th>Acciones</th>
+            </tr>   
+            </thead> 
+            <tbody>
+                @foreach($alumnos as $alumno)
+                <tr>
+                    <td>{{$alumno->matricula}}</td>
+                    <td>{{$alumno->nombre}}</td>
+                    <td>{{$alumno->apellido}}</td>
+                    <td>{{$alumno->carrera}}</td>
+                    <td>{{$alumno->correo_electronico}}</td>
+                    <td>
+                        <a href="#" class="btn btn-success edit">Editar</a>
+                        <a href="#" class="btn btn-danger delete">Borrar</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        </div>
+    </div>
+@endsection
+
+@section('editarborrar')
+<script type="text/javascript">
+            $(document).ready(function() {
+
+                var table= $('#alumnos').DataTable({
+                    responsive: true,
+                    autoWidth: false,
+                    "language": {
+                        "lengthMenu": "Mostrar _MENU_ registros por página",
+                        "zeroRecords": "Nada encontrado - disculpa",
+                        "info": "Mostrando la página _PAGE_ de _PAGES_",
+                        "infoEmpty": "Registros no disponibles",
+                        "infoFiltered": "(flitrado de _MAX_ registros totales)",
+                        "search": "Buscar",
+                        "paginate": {
+                            "next": "Siguiente",
+                            "previous": "Anterior"
+                    }}
+                    });
+                    //start edit record 
+                    table.on('click', '.edit', function(){
+
+                        $tr = $(this).closest('tr');
+                        if ($($tr).hasClass('child')){
+                            $tr =  $tr.prev('.parent');
+                        }
+                        var data = table.row($tr).data();
+                        console.log(data);
+
+                        $('#matricula').val(data[0]);
+                        $('#nombre').val(data[1]);
+                        $('#apellido').val(data[2]);
+                        $('#carrera').val(data[3]);
+                        $('#correo_electronico').val(data[4]);
+
+                        $('#editForm').attr('route', 'editaralumno', + data[0]);
+                        $('#editModal').modal('show');
+                    });
+
+                    //end edit record 
+
+                    //star delete record 
+                    table.on('click', '.delete', function(){
+
+                        $tr = $(this).closest('tr');
+                        if ($($tr).hasClass('child')){
+                            $tr =  $tr.prev('.parent');
+                        }
+                        var data = table.row($tr).data();
+                        console.log(data);
+
+                        $('#matricula').val(data[0]);
+
+                        $('#deleteForm').attr('route', 'borraralumno', + data[0]);
+                        $('#deleteModal').modal('show');
+                    });
+                    //end delete record 
+                });
+        </script>
+@endsection
---
+++ centros_computo_2/resources/views/layouts/plantillabase2.blade.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Gestión de {{$nombrePag}}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="./img/core-img/favicon.png">
+
+    <!-- Stylesheet -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href=" https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+   
+</head>
+
+<body>
+    <!-- Header Area Start -->
+    <header class="header-area">
+        <!-- Search Form -->
+        <div class="search-form d-flex align-items-center">
+            <div class="container">
+                <form action="index.html" method="get">
+                    <input type="search" name="search-form-input" id="searchFormInput" placeholder="Type your keyword ...">
+                    <button type="submit"><i class="icon_search"></i></button>
+                </form>
+            </div>
+        </div>
+
+        <!-- Top Header Area Start -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-6">
+                        <div class="top-header-content">
+                            <a href="#"><i class="icon_phone"></i> <span>(123) 456-789-1230</span></a>
+                            <a href="#"><i class="icon_mail"></i> <span>info.colorlib@gmail.com</span></a>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="top-header-content">
+                            <!-- Top Social Area -->
+                            <div class="top-social-area ml-auto">
+                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- Top Header Area End -->
+
+        <!-- Main Header Start -->
+        <div class="main-header-area">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Classy Menu -->
+                    <nav class="classy-navbar justify-content-between" id="robertoNav">
+
+                        <!-- Logo -->
+                        <a class="nav-brand" href="index.html"><img src="./img/core-img/logo-buap.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+                            <!-- Menu Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul id="nav">
+                                    <li class="active"><a href="{{route('index')}}">Inicio</a></li>
+                                    <li><a href="{{route('fallaP')}}">Fallas</a></li>
+                                    <li><a href="{{route('faltaP')}}">Faltas</a></li>
+                                    <li><a href="{{route('usuariosP')}}">Prestamos</a></li>
+                                    <li><a href="{{route('materiasP')}}">Software</a></li>
+                                    <li><a href="{{route('materiasP')}}">Horarios</a></li>
+                                </ul>
+
+                                <!-- Search -->
+                                <div class="search-btn ml-4">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Breadcrumb Area Start -->
+    <div class="breadcrumb-area bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/17.jpg)">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="breadcrumb-content text-center">
+                        <h2 class="page-title">{{$nombrePag}}</h2>
+                        
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb justify-content-center">
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        @yield('contenido')
+    </div>
+
+    <!-- Partner Area Start -->
+    <div class="partner-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="partner-logo-content d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="300ms">
+                        <!-- Single Partner Logo -->
+                        <a href="#" class="partner-logo"><img src="img/core-img/p1.png" alt=""></a>
+                        <!-- Single Partner Logo -->
+                        <a href="#" class="partner-logo"><img src="img/core-img/p2.png" alt=""></a>
+                        <!-- Single Partner Logo -->
+                        <a href="#" class="partner-logo"><img src="img/core-img/p3.png" alt=""></a>
+                        <!-- Single Partner Logo -->
+                        <a href="#" class="partner-logo"><img src="img/core-img/p4.png" alt=""></a>
+                        <!-- Single Partner Logo -->
+                        <a href="#" class="partner-logo"><img src="img/core-img/p5.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Partner Area End -->
+
+    <!-- Footer Area Start -->
+    <footer class="footer-area section-padding-80-0">
+        <!-- Main Footer Area -->
+        <div class="main-footer-area">
+            <div class="container">
+                <div class="row align-items-baseline justify-content-between">
+                    <!-- Single Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <div class="single-footer-widget mb-80">
+                            <!-- Footer Logo -->
+                            <a href="#" class="footer-logo"><img src="img/core-img/logo-buap.png" alt=""></a>
+
+                            <h4>+12 345-678-9999</h4>
+                            <span>Info.colorlib@gmail.com</span>
+                            <span>856 Cordia Extension Apt. 356, Lake Deangeloburgh, South Africa</span>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget Area -->
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <div class="single-footer-widget mb-80">
+                            <!-- Widget Title -->
+                            <h5 class="widget-title">Our Blog</h5>
+
+                            <!-- Single Blog Area -->
+                            <div class="latest-blog-area">
+                                <a href="#" class="post-title">Freelance Design Tricks How</a>
+                                <span class="post-date"><i class="fa fa-clock-o" aria-hidden="true"></i> Jan 02, 2019</span>
+                            </div>
+
+                            <!-- Single Blog Area -->
+                            <div class="latest-blog-area">
+                                <a href="#" class="post-title">Free Advertising For Your Online</a>
+                                <span class="post-date"><i class="fa fa-clock-o" aria-hidden="true"></i> Jan 02, 2019</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget Area -->
+                    <div class="col-12 col-sm-4 col-lg-2">
+                        <div class="single-footer-widget mb-80">
+                            <!-- Widget Title -->
+                            <h5 class="widget-title">Links</h5>
+
+                            <!-- Footer Nav -->
+                            <ul class="footer-nav">
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> About Us</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> Our Room</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> Career</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> FAQs</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget Area -->
+                    <div class="col-12 col-sm-8 col-lg-4">
+                        <div class="single-footer-widget mb-80">
+                            <!-- Widget Title -->
+                            <h5 class="widget-title">Subscribe Newsletter</h5>
+                            <span>Subscribe our newsletter gor get notification about new updates.</span>
+
+                            <!-- Newsletter Form -->
+                            <form action="index.html" class="nl-form">
+                                <input type="email" class="form-control" placeholder="Enter your email...">
+                                <button type="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copywrite Area -->
+        <div class="container">
+            <div class="copywrite-content">
+                <div class="row align-items-center">
+                    <div class="col-12 col-md-8">
+                        <!-- Copywrite Text -->
+                        <div class="copywrite-text">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <!-- Social Info -->
+                        <div class="social-info">
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Area End -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+    </script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+    <div class="container">
+        @yield('editarborrar')
+    </div>   
+</body>
+</html>
