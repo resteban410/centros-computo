585778e69cb7c247ac7063b52189e97bbcd6748b
dorsai0611 <41398776+dorsai0611@users.noreply.github.com>
2021-10-14 22:52:03 -0500

    Orden en el repositorio

--- centros_computo_FER/resources/js/agenda.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('agenda');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-    	initialView: 'dayGridMonth'
-        });
-      calendar.render();
-    });
