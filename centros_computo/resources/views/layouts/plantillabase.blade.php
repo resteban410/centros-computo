25053152fb6e1c6355bb7f36c8d5356716b43c60
dorsai0611 <41398776+dorsai0611@users.noreply.github.com>
2021-10-19 23:06:59 -0500

    Removing the bootstrap request to Json styles

--- centros_computo/resources/views/layouts/plantillabase.blade.php
+++ centros_computo/resources/views/layouts/plantillabase.blade.php
@@ -32,8 +32,6 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href=" https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-
    
     
 </head>
