97b59c0d5aed5946a7f81d16d3f4ba67e8a412de
fernandalvarezarenas <fernanda.alvarez.arenas@gmail.com>
2022-01-07 23:33:57 -0600

    Actualizacion composer

--- centros_computo/app/Models/UsuarioModel.php
+++ centros_computo/app/Models/UsuarioModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\ Foundation\ Auth\ User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UsuarioModel extends Authenticatable
 {
