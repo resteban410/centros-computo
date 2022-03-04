d402f4f4dc1b62fc4fd0198bf8ce6904f67c4da2
resteban410 <resteban410@gmail.com>
2022-03-03 22:32:21 -0600

    Plantilla de correo

--- centros_computo/app/Mail/ContactMail.php
+++ centros_computo/app/Mail/ContactMail.php
@@ -29,7 +29,7 @@ class ContactMail extends Mailable
      */
     public function build()
     {
-        return $this->subject('Contact Message')->view('emails.ContactMail');
+        return $this->subject('Correo SJCH')->view('emails.ContactMail');
 
     }
 }
